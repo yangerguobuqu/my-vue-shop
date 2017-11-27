cfd60dfe32eccdbef9cde7b607221fd311acea9c
yangerguobuqu <caoxiaoning1988@163.com>
2017-11-28 02:23:16 +0800

    服务端允许跨域配置（server/app.js文件CORS配置）

--- server/app.js
+++ server/app.js
@@ -4,15 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var ejs = require("ejs");
 var index = require('./routes/index');
-var users = require('./routes/users');
+var goods = require('./routes/goods');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine(".html",ejs.__express);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +23,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.all("*",function(req,res,next){
+//  CORS≈‰÷√
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By","3.2.1");
+  res.header("Content-Type","application/json;charset=utf-8");
+  next();
+})
 app.use('/', index);
-app.use('/users', users);
+app.use('/goods', goods);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
