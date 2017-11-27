bbf20068cbd2d6c27c1c222abb191069d5c79b88
yangerguobuqu <caoxiaoning1988@163.com>
2017-11-28 01:29:58 +0800

    连接mongodb商品列表查询

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require("mongoose");
+var Goods= require("./../models/goods")
 
 //连接数据库
-
 mongoose.connect('mongodb://127.0.0.1:27017/vue_shop');
 mongoose.connection.on("connected",function(){
   console.log("mongodb connected successfully !")
@@ -16,7 +16,23 @@ mongoose.connection.on("disconnected",function(){
 })
 /* 查询商品列表数据 */
 router.get('/', function(req, res, next) {
-  res.send('欢迎来到 users 页面');
+  Goods.find({},function(err,doc){
+      if(err){
+        res.json({
+          status:'1',
+          msg:err.message
+        })
+      }else{
+        res.json({
+          status:'0',
+          msg:'',
+          result:{
+            count:doc.length,
+            list:doc
+          }
+        })
+      }
+  })
 });
 
 module.exports = router;
