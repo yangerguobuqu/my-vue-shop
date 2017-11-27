7792087758ea0e0404207ea17cbfafafd09ea40c
yangerguobuqu <caoxiaoning1988@163.com>
2017-11-27 23:39:30 +0800

    引入vue-lazyload插件，实现图片懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,19 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/cart'
-
+import VueLazyload from "vue-lazyload"
 Vue.use(Router)
+Vue.use(VueLazyload, {
+  loading: 'static/loading-svg/loading-bars.svg',
+  try: 3 // default 1
+})
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'GoodsList',
+      component: GoodsList
     },
     {
       path: '/GoodsList',
